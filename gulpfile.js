27c158aa7fa9f48b751b540efa9e302990196524
Taiga KIYOKAWA <taiga.k127@gmail.com>
2019-04-09 04:30:42 +0900

    rewrite index.html

--- gulpfile.js
+++ gulpfile.js
@@ -5,16 +5,25 @@ var autoprefixer = require('gulp-autoprefixer');
 var browserSync = require("browser-sync");
 
  
-gulp.task('default', ['sass']);
+gulp.task('server', function(done) {
+  return browserSync.init({
+    server: {
+      baseDir: '.'
+    }
+  })
+  done();
+})
 
-gulp.task('watch', ['server'], function () {
-	gulp.watch('./css/**/*.scss', ['sass']);
-	gulp.watch('./css/**/*.css', ['bs-reload']);
-	gulp.watch('./*.html', ['bs-reload']);
-	gulp.watch('./script/**/*.js', ['bs-reload']);
+gulp.task('watch', gulp.task('server'), function (done) {
+	gulp.watch('./css/**/*.scss', gulp.task('sass'));
+	gulp.watch('./css/**/*.css', gulp.task('bs-reload'));
+	gulp.watch('./*.html', gulp.task('bs-reload'));
+  gulp.watch('./script/**/*.js', gulp.task('bs-reload'));
+  
+  done();
 });
 
-gulp.task('sass', function () {
+gulp.task('sass', function (done) {
 	gulp.src('./css/**/*.scss')
 		.pipe(sourcemaps.init())
 		.pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
@@ -22,17 +31,15 @@ gulp.task('sass', function () {
 		// .pipe(sourcemaps.init({loadMaps: true}))
 		.pipe(autoprefixer(['last 3 versions', 'ie >= 8', 'Android >= 4', 'iOS >= 8']))
 		// .pipe(sourcemaps.write())
-		.pipe(gulp.dest('./css/'));
+    .pipe(gulp.dest('./css/'));
+
+    done();
 });
 
-gulp.task('server', function() {
-  return browserSync.init({
-    server: {
-      baseDir: '.'
-    }
-  })
+
+gulp.task('bs-reload', function(done) {
+  browserSync.reload();
+  done();
 })
 
-gulp.task('bs-reload', function() {
-  browserSync.reload();
-})
+gulp.task('default', gulp.task('sass'));
